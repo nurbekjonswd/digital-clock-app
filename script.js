b99c00cd731654258e4b6210ade431caf7e446e4
cleaverprogrammer <nurbek.malikov199711@gmail.com>
2025-04-29 11:05:22 +0500

    Digital Clock project

---
+++ script.js
@@ -0,0 +1,12 @@
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+const ampm = document.getElementById("ampm");
+// this code for updating clock every single second
+
+function updateClock() {
+  let now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+}
